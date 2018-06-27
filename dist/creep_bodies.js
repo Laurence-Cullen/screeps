d61ff0da923531da465708ed5a740f88c327976c
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-06-27 21:21:57 +0100

    removing commented out hardcoded bodies

--- dist/creep_bodies.js
+++ dist/creep_bodies.js
@@ -3,34 +3,6 @@ bodyUtils = require('body_utils');
 
 
 module.exports = {
-    // light: bodyUtils.construct_body({
-    //     'work': 1,
-    //     'carry': 1,
-    //     'move': 1
-    // }),
-    // medium: bodyUtils.construct_body({
-    //     'work': 2,
-    //     'carry': 1,
-    //     'move': 1
-    // }),
-    // large: bodyUtils.construct_body({
-    //     'work': 2,
-    //     'carry': 2,
-    //     'move': 2
-    // }),
-    // extra_large: bodyUtils.construct_body({
-    //     'work': 4,
-    //     'carry': 4,
-    //     'move': 4
-    // }),
-    // fast_melee: bodyUtils.construct_body({
-    //     'attack': 6,
-    //     'move': 6
-    // }),
-    // fast_ranged: bodyUtils.construct_body({
-    //     'ranged_attack': 4,
-    //     'move': 4
-    // })
     max_labourer: function (energy_budget) {
         // calculate how many multiples of 1 move, 1 work and 1 carry can fit within energy budget
         const body_parts = _.floor(energy_budget / (part_costs.move + part_costs.work + part_costs.carry));

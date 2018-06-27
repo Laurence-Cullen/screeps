de007689fe1401a323e42ebd5da09fc0f6a5b0de
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-06-28 00:33:32 +0100

    reordered spawn commands to come after economic commands to stop economy freezing when spawnManager crashes

--- dist/main.js
+++ dist/main.js
@@ -5,13 +5,13 @@ const memoryCleaner = require('memory_cleaner');
 module.exports.loop = function () {
     console.log('TICK');
 
+    // issue creep run commands
+    workDispatcher.run();
+
     // clean up memory of dead creeps
     memoryCleaner.clean();
 
     // spawn more creeps if necessary
     spawnManager.military();
     spawnManager.economic();
-
-    // issue creep run commands
-    workDispatcher.run();
 };

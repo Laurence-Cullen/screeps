a000e958ba33d10bcf0846cbd4b106a0110974c9
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-06-29 18:55:29 +0100

    moved source code from dest -> src and added automatic file system flattening to allow for use of folders in source code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,51 @@
 module.exports = function (grunt) {
-    let config = require('./.screeps.json');
+
+    const config = require('./.screeps.json');
+    const branch = grunt.option('branch') || config.branch;
+    const email = grunt.option('email') || config.email;
+    const password = grunt.option('password') || config.password;
+    const ptr = grunt.option('ptr') ? true : config.ptr;
+
     grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
     grunt.initConfig({
         screeps: {
             options: {
-                email: config.email,
-                password: config.password,
-                branch: config.branch,
-                ptr: config.ptr
+                email: email,
+                password: password,
+                branch: branch,
+                ptr: ptr
             },
             dist: {
                 src: ['dist/*.js']
             }
-        }
+        },
+
+        // Remove all files from the dist folder.
+        clean: {
+            'dist': ['dist']
+        },
+
+        // Copy all source files into the dist folder, flattening the folder structure by converting path delimiters to underscores
+        copy: {
+            // Pushes the game code to the dist folder so it can be modified before being send to the screeps server.
+            screeps: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: '**',
+                    dest: 'dist/',
+                    filter: 'isFile',
+                    rename: function (dest, src) {
+                        // Change the path name utilize underscores for folders
+                        return dest + src.replace(/\//g, '_');
+                    }
+                }],
+            }
+        },
     });
+
+    grunt.registerTask('default', ['clean', 'copy:screeps', 'screeps']);
 };
